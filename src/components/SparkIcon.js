70c81f3fc4fe8b276add3b4b350c368053520646
lucasbarcellos <lucas.araujo@deliveryhero.com>
2025-04-30 09:25:53 +0200

    Wip

--- src/components/SparkIcon.js
+++ src/components/SparkIcon.js
@@ -1,7 +1,7 @@
 // Create a separate SparkIcon component for reusability
 import React from 'react';
 
-const SparkIcon = ({ width = 32, height = 32, fill = "#df1067" }) => (
+const SparkIcon = ({ width = 32, height = 32, fill = "#FF2B85" }) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     enableBackground="new 0 0 24 24" 
