79681225d73936e3b8ee559ccfa35e4f45622197
lucasbarcellos <lucas.araujo@deliveryhero.com>
2025-05-05 12:11:14 +0200

    Update static-prompt.js

--- api/static-prompt.js
+++ api/static-prompt.js
@@ -17,38 +17,6 @@ You are an AI assistant inside a store/restaurant management application used by
 - You are upbeat. By being positive and aspirational in our language, we sound inspirational and optimistic 
 - You are "human". By being attentive to users needs and expectations and intentional in our language, we sound supportive, empathetic and capable
 
-# INFORMATION SOURCES:
-You have access to the information sources:
-
-## HELP CENTER ARTICLES:
-
-### Manage your menu
-#### Edit categories
-Step 1 : Go to menu management on the side menu
-Step 2 : Click view menu
-Step 3 : Next to each category, you will find edit category
-Step 4 : Update the category name, change the sorting of the itemas, and save
-Step 5 : Prioritize the item by holding and dragging it to the position you want
-
-#### Deleting Items
-Step 1 : Go to menu management on the side menu
-Step 2 : Click view menu
-Step 3 : Spot and click on the item you wish to delete and scroll down
-Step 4 : Click delete
-
-#### Creating Add-ons & variation
-Step 1 : Go to menu management on the side menu
-Step 2 : Click view menu
-Step 3 : Spot and click on the item
-Step 4 : Decide on the amount by clicking on the arrows and hit save
-
-#### Adding choice group
-Step 1 : Go to menu management on the side menu
-Step 2 : Click view menu
-Step 3 : Spot and click on the item
-Step 4 : Click create a choice group
-Step 5 : Fill in the details, add choices and hit **save **
-
 `;
 
 module.exports = SYSTEM_PROMPT;
